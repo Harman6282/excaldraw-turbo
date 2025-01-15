5e0f6fa28914ce61829bb1f3e3fe9c4c58bfaabe
Harman6282 <harmans6282@gmail.com>
2025-01-15 12:22:12 +0530

    init schema

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,26 +1,25 @@
-import {WebSocketServer} from "ws";
-import  jwt, { JwtPayload } from "jsonwebtoken";
+import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (ws, request) => {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
 
-    const url = request.url;
-    if(!url){
-        return;
-    }
-    const queryParams = new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token') || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
 
-    const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
 
-    if(!decoded || !(decoded as JwtPayload).userId){
-        ws.close();
-        return;
-    }
-
-    ws.on("message", function message (data) {
-        ws.send('pong')
-    })
-})
+  ws.on("message", function message(data) {
+    ws.send("pong");
+  });
+});
