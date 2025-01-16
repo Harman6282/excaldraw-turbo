6705e66eb5fc9961a92cfa5f17479c082856b52b
Harman6282 <harmans6282@gmail.com>
2025-01-16 10:22:28 +0530

    backend done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -109,23 +109,20 @@ app.post("/room", middleware, async (req, res) => {
   }
 });
 
-
 app.get("/chats/:roomId", async (req, res) => {
   const roomId = Number(req.params.roomId);
   const messages = await prismaClient.chat.findMany({
-    where:{
-      roomId: roomId
+    where: {
+      roomId: roomId,
     },
-    orderBy:{
-      id: "desc"
+    orderBy: {
+      id: "desc",
     },
-    take: 50
-  })
+    take: 50,
+  });
 
-  res.json({messages})
-
-})
-
+  res.json({ messages });
+});
 
 app.listen(3007, () => {
   console.log("http-backend listening on port 3004");
